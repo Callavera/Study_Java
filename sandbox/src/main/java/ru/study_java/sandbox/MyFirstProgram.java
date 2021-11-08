0e06eda63381ba377de835efa744737a1c14efda
Callavera <manny.calavera2021@yandex.ru>
2021-11-08 14:49:24 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/study_java/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/study_java/sandbox/MyFirstProgram.java
@@ -3,6 +3,27 @@ package ru.study_java.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		hello("World");
+		hello("Alex");
+		double l = 5;
+		System.out.println("Площадь:"  + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь: " + area(a, b));
+
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello," + somebody + "!");
+	}
+
+	public static double area(double l) {
+		return l * l;
+	}
+
+
+	public static double area(double a, double b) {
+		return a * b;
 	}
 }
